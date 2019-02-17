195613b4978cfad67954de28712a9179f4834b00
Darcy LF <ehnevault@gmail.com>
2019-02-17 12:23:27 +1100

    add summary cards

--- script.js
+++ script.js
@@ -38,6 +38,9 @@
         var rain = (data.currently.precipProbability * 100).toFixed(0) + " %";
         var uvIndex = data.currently.uvIndex;
         
+        var summary = data.hourly.summary;
+        var weekSummary = data.daily.summary;
+        
         for (i = 0; i < data.hourly.data.length; i++) {
               forecastData.highTemp[i] = data.hourly.data[i].temperature;
               console.log(data.hourly.data[i].temperature);
@@ -53,6 +56,10 @@
           $("#rain").html(rain)
           $("#uv").html(uvIndex)
           
+          $("#summary").html(summary)
+          $("#weekly-summary").html(weekSummary)
+          
+          
         //today forecast in C
         var todayMaxTemp = data.daily.data[0].temperatureMax.toFixed(0);
         var todayMinTemp = data.daily.data[0].temperatureMin.toFixed(0);
@@ -61,43 +68,7 @@
                 
                 
               
-                var myChart = new Chart(ctx, {
-                    type: 'line',
-                    data: {
-                        labels: ['12 am','1 am','2 am','3 am','4 am','5 am','6 am','7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm','8 pm','9 pm','10 pm','11 pm','12 pm'],
-                        datasets: [{
-                            label: 'Temperature',
-                            data: forecastData.highTemp,
-                            backgroundColor: [
-                                'rgba(255,43,0, 0.2)',
-                            ],
-                            borderColor: [
-                                'rgba(255,43,0,1)',
-                            ],
-                            borderWidth: 1
-                        }]
-                    },
-                    options: {
-                        title: {
-                            display: true,
-                            text: "Todays Temperature",
-                            fontSize: 17,
-                            fontColor: "#000",
-                        },
-                        layout: {
-                            padding: {
-                                left: 0,
-                                right: 3,
-                                top: 0,
-                                bottom: 0
-                            }
-                        },
-                        legend: {
-                            display: false,
-                        }
-                    }
-                });
-        
+                        
         
         
         
