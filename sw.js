970960cb47f32a5a9977b6700bd35e169fe5940b
Darcy LF <ehnevault@gmail.com>
2019-02-17 12:49:51 +1100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
 // we'll version our cache (and learn how to delete caches in
 // some other post)
 const cacheName = 'v2::static';
+caches.delete('v1::static');
+
 
 self.addEventListener('install', e => {
   // once the SW is installed, go ahead and fetch the resources
